526918a48b45bf4aa38d4585d75695c29da1921e
vashura <valerio.lopez@outlook.com>
2024-02-10 00:40:21 -0600

    actualizacion de proyecto spring:actualmente solo contiene configuraciones basicas

---
+++ src/IoC/JefeEmpleado.java
@@ -0,0 +1,8 @@
+package IoC;
+
+public class JefeEmpleado {
+
+	public String getTareas() {
+		return "Gestiono las cuestiones relativas a mis empleados de seccion";
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProyectoSpringUno {
+}
